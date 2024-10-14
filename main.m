3896d9ea1c19465dfa236ac37c14239834aaf198
huberywooooo <hbw8456@163.com>
2024-10-14 14:14:15 +0800

    Create main.m

---
+++ main.m
@@ -0,0 +1,14 @@
+%%%%-----------------------------openStruc--------------------------------------
+%
+%       Description:
+
+%
+%   openStruc: An Open Source Library for Structure mechanism
+%   Author(s): Hubery H.B. Woo (hbw8456@163.com)
+%   Copyright 2023-2024 Chongqing Three Gorges University
+
+
+function main()
+	disp("unfinished.");
+
+end
